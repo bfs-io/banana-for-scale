07fac7d7bb01fece445c8768a1f3ced83de19d7d
Mike Hacker <mhackersu@gmail.com>
2022-07-22 11:15:02 -0500

    adding buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,12 +34,15 @@ class IndexPage extends React.Component {
           <Button marginTop="35px">Join The Conversation on Slack</Button>
         </a>
         <a href="https://github.com/bananaforscales" target="_blank">
-          <Button marginTop="35px">Join Our GitHub Org</Button>
+          <Button marginTop="35px">Join us on GitHub</Button>
         </a>
         <h1>We are sponsoring a fund-raising effort for the Foster for CS program</h1>
         <img src="https://res.cloudinary.com/desertsofcacti/image/upload/c_scale,w_800/v1658200195/learn-cs4foster-tee_ath9ge.png"/>
         <a href="https://www.bonfire.com/learn-8/" target="_blank">
-          <Button marginTop="35px">Sponser CS for Foster</Button>
+          <Button marginTop="35px">Sponser Foster CS</Button>
+        </a>
+        <a href="https://github.com/fostercs/curriculum" target="_blank">
+          <Button marginTop="35px">Learn More about Foster CS Here</Button>
         </a>
       </Layout>
     )
